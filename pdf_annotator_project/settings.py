fceb09ce50b78d8011266d0f0716e33981e7088b
“Elanchezhian2712” <elanchezhian2712.m@gmail.com>
2025-04-21 12:39:11 +0530

    changes

--- pdf_annotator_project/settings.py
+++ pdf_annotator_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(48^lmt9da!-!ygo%47wv8%z%==grwv14ewmmdpba#u0!zq(++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
